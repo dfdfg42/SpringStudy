367620f6c1e4ae1c910ba19b683dcd4c432b0604
고경우 <dfdfg11@gmail.com>
2024-02-23 20:35:55 +0900

    .

--- springBoardStudy/src/main/java/com/example/CSEC/site/question/QuestionController.java
+++ springBoardStudy/src/main/java/com/example/CSEC/site/question/QuestionController.java
@@ -1,9 +1,12 @@
 package com.example.CSEC.site.question;
 
 
+import com.example.CSEC.site.answer.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,20 +25,23 @@ public class QuestionController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id){
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm){
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
     }
 
     @GetMapping("/create")
-    public String questionCreate(){
+    public String questionCreate(QuestionForm questionForm){
         return "question_form";
     }
 
     @PostMapping("/create")
-    public String questionCreate(@RequestParam(value = "subject") String subject, @RequestParam(value="content") String content ){
-        this.questionService.create(subject,content);
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list";
     }
 }
