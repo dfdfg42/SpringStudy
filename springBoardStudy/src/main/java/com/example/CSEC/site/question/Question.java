0071d66a4532a169b017eb8fbc51a50d0ec1325b
고경우 <dfdfg11@gmail.com>
2024-02-25 13:54:37 +0900

    .

--- springBoardStudy/src/main/java/com/example/CSEC/site/question/Question.java
+++ springBoardStudy/src/main/java/com/example/CSEC/site/question/Question.java
@@ -2,6 +2,7 @@ package com.example.CSEC.site.question;
 
 
 import com.example.CSEC.site.answer.Answer;
+import com.example.CSEC.site.user.SiteUser;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,8 +25,10 @@ public class Question {
     @Column(columnDefinition =  "TEXT")
     private String content;
 
-
     private LocalDateTime createDate;
+
+    @ManyToOne
+    private SiteUser author;
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
