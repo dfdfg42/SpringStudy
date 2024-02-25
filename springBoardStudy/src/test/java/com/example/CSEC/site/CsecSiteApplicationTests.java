366c4871bdd85baedc099c145afbe2c3bc515774
고경우 <dfdfg11@gmail.com>
2024-02-25 17:03:13 +0900

    .

--- springBoardStudy/src/test/java/com/example/CSEC/site/CsecSiteApplicationTests.java
+++ springBoardStudy/src/test/java/com/example/CSEC/site/CsecSiteApplicationTests.java
@@ -13,22 +13,4 @@ import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
-@Transactional
-class CsecSiteApplicationTests {
 
-	@Autowired
-	private QuestionService questionService;
-
-	@Test
-	void testJpa(){
-		for (int i = 1; i <= 300; i++) {
-			String subject = String.format("테스트 데이터입니다:[%03d]", i);
-			String content = "내용무";
-			this.questionService.create(subject, content , null);
-
-
-		}
-	}
-
-}
