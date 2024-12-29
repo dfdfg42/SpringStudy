c3d0a9f2ef582a128c41faf95ebceb22aebc84e0
고경우 <dfdfg11@gmail.com>
2024-12-29 12:55:12 +0900

    mvc-controllerv4

--- springMvc1Practice/src/main/java/hello/servlet/domain/member/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
+++ springMvc1Practice/src/main/java/hello/servlet/domain/member/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -8,18 +8,15 @@ import hello.servlet.domain.member.web.frontcontroller.v3.ControllerV3;
 import java.util.Map;
 
 public class MemberSaveControllerV3 implements ControllerV3 {
-
-
     private MemberRepository memberRepository = MemberRepository.getInstance();
     @Override
     public ModelView process(Map<String, String> paramMap) {
         String username = paramMap.get("username");
         int age = Integer.parseInt(paramMap.get("age"));
-
         Member member = new Member(username, age);
-
-        ModelView mv = new ModelView("save-reuslt");
-        mv.getModel().put("memger",member);
+        memberRepository.save(member);
+        ModelView mv = new ModelView("save-result");
+        mv.getModel().put("member", member);
         return mv;
     }
 }
